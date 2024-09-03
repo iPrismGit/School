b37cdf6f954a47424e617e25e37e578fc0eea8ad
Omprakash <omprakash.mopuri@improve10x.com>
2024-09-03 14:14:50 +0530

    Completed some design

--- app/src/main/java/com/iprism/school/activities/LoginActivity.kt
+++ app/src/main/java/com/iprism/school/activities/LoginActivity.kt
@@ -1,12 +1,26 @@
 package com.iprism.school.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.iprism.school.R
+import com.iprism.school.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        handleRequestOtp()
+    }
+
+    private fun handleRequestOtp() {
+        binding.requestOtpBtn.setOnClickListener(View.OnClickListener {
+            startActivity(Intent(this, OtpVerificationActivity::class.java))
+        })
     }
 }
