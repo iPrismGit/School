e0dd2bc7eeb92b9d28a0126e8801cc8e5e29c93a
Omprakash <omprakash.mopuri@improve10x.com>
2024-09-03 14:24:16 +0530

    Completed some design

--- app/src/main/java/com/iprism/school/activities/OtpVerificationActivity.kt
+++ app/src/main/java/com/iprism/school/activities/OtpVerificationActivity.kt
@@ -1,12 +1,26 @@
 package com.iprism.school.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.iprism.school.R
+import com.iprism.school.databinding.ActivityOtpVerificationBinding
 
 class OtpVerificationActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityOtpVerificationBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_otp_verification)
+        binding = ActivityOtpVerificationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        handleContinueBtn()
+    }
+
+    private fun handleContinueBtn() {
+        binding.continueBtn.setOnClickListener(View.OnClickListener {
+            //startActivity(Intent(this, MainActivity::class.java))
+        })
     }
 }
