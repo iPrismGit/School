cd256cebb04acb50dbf73039c2bef3ce9176c7ad
Omprakash <omprakash.mopuri@improve10x.com>
2024-09-03 17:29:28 +0530

    Completed home screen quick actions design

--- app/src/main/java/com/iprism/school/activities/HomeActivity.kt
+++ app/src/main/java/com/iprism/school/activities/HomeActivity.kt
@@ -2,11 +2,52 @@ package com.iprism.school.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.iprism.school.R
+import com.iprism.school.databinding.ActivityHomeBinding
+import com.iprism.school.fragments.HomeFragment
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        handleBottomNav()
+        switchFragment(HomeFragment())
+    }
+
+    private fun handleBottomNav() {
+        binding.bottomNavigationView.setOnItemSelectedListener() { item ->
+            when(item.itemId) {
+                R.id.home -> {
+                    switchFragment(HomeFragment())
+                    true
+                }
+                R.id.messages -> {
+                    // Respond to navigation item 2 click
+                    true
+                }R.id.messages -> {
+                // Respond to navigation item 2 click
+                true
+                }R.id.messages -> {
+                // Respond to navigation item 2 click
+                true
+                }R.id.messages -> {
+                // Respond to navigation item 2 click
+                true
+                }
+                else -> false
+            }
+        }
+    }
+
+    private fun switchFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.flFragment, fragment)
+            .commit();
     }
 }
