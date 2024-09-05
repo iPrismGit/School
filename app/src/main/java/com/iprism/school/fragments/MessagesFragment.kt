171899169fc8c2a5d7aad4c32bd00b4488f0c4a9
Omprakash <omprakash.mopuri@improve10x.com>
2024-09-05 11:59:53 +0530

    Completed MessageActivity.kt design

--- app/src/main/java/com/iprism/school/fragments/MessagesFragment.kt
+++ app/src/main/java/com/iprism/school/fragments/MessagesFragment.kt
@@ -1,19 +1,28 @@
 package com.iprism.school.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.iprism.school.R
+import com.iprism.school.activities.MessageActivity
+import com.iprism.school.databinding.FragmentMessagesBinding
 
 class MessagesFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_messages, container, false)
+    private lateinit var binding : FragmentMessagesBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentMessagesBinding.inflate(inflater)
+        handleMessageBtn()
+        return binding.root
+    }
+
+    private fun handleMessageBtn() {
+        binding.messageBtn.setOnClickListener(View.OnClickListener {
+            startActivity(Intent(context, MessageActivity::class.java))
+        })
     }
 }
