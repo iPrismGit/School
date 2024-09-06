c698c66a42767d0c94cb64299839c31264910577
Omprakash <omprakash.mopuri@improve10x.com>
2024-09-06 14:29:30 +0530

    Completed side nav

--- app/src/main/java/com/iprism/school/fragments/HomeFragment.kt
+++ app/src/main/java/com/iprism/school/fragments/HomeFragment.kt
@@ -2,13 +2,12 @@ package com.iprism.school.fragments
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.iprism.school.R
 import com.iprism.school.activities.StudentsActivity
-import com.iprism.school.databinding.ActivityHomeBinding
 import com.iprism.school.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -18,6 +17,13 @@ class HomeFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = FragmentHomeBinding.inflate(layoutInflater)
         handleStudentsLL()
+        binding.menuImg.setOnClickListener(View.OnClickListener {
+            if (binding.drawer.isDrawerOpen(Gravity.LEFT)) {
+                binding.drawer.closeDrawer(Gravity.LEFT)
+            } else {
+                binding.drawer.openDrawer(Gravity.LEFT)
+            }
+        })
         return binding.root
     }
 
