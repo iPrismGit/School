163e113bea3cafb8cd5dbce81a351eb2eea9ef2f
CHARAN TEJA <charan.teja@improve10x.com>
2024-09-18 10:43:40 +0530

    Created OnConsentItemClickListener interface class and implemented that

--- app/src/main/java/com/iprism/school/activities/SingleConsentActivity.kt
+++ app/src/main/java/com/iprism/school/activities/SingleConsentActivity.kt
@@ -5,22 +5,41 @@ import android.os.Bundle
 import android.view.View
 import com.iprism.school.R
 import com.iprism.school.databinding.ActivitySingleConsentBinding
-import com.iprism.school.databinding.ConsentItemBinding
 
 class SingleConsentActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySingleConsentBinding
+    private var isInfoVisible: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySingleConsentBinding.inflate(layoutInflater)
         setContentView(binding.root)
         handleBack()
+        handleDownArrow()
+    }
+
+    private fun handleDownArrow() {
+        binding.dropDownIv.setOnClickListener(View.OnClickListener {
+            toggleInformationVisibility()
+        })
     }
 
     private fun handleBack() {
         binding.backIv.setOnClickListener(View.OnClickListener {
             finish()
         })
+    }
+
+    private fun toggleInformationVisibility() {
+        if (isInfoVisible) {
+            binding.infoLo.visibility = View.GONE
+            binding.dropDownIv.setImageResource(R.drawable.down_arrow_img)
+        } else {
+            binding.infoLo.visibility = View.VISIBLE
+            binding.dropDownIv.setImageResource(R.drawable.up_arrow_img)
+        }
+        isInfoVisible = !isInfoVisible
     }
 
 }
