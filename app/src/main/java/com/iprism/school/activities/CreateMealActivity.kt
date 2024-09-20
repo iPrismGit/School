726d104cd232f4d62b757760a1cdb5c96bc5adf2
CHARAN TEJA <charan.teja@improve10x.com>
2024-09-20 09:56:35 +0530

    Designed Meal Planner Module

--- app/src/main/java/com/iprism/school/activities/CreateMealActivity.kt
+++ app/src/main/java/com/iprism/school/activities/CreateMealActivity.kt
@@ -2,14 +2,21 @@ package com.iprism.school.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.Toast
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.iprism.school.R
 import com.iprism.school.databinding.ActivityCreateMealBinding
 
 class CreateMealActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityCreateMealBinding
+    private lateinit var crossImage: ImageView
+    private lateinit var okBtn: Button
+    private lateinit var cancelBtn: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +24,13 @@ class CreateMealActivity : AppCompatActivity() {
         setContentView(binding.root)
         handleBack()
         handleSubmitBtn()
+        handleFoodTypeLo()
+    }
+
+    private fun handleFoodTypeLo() {
+        binding.foodTypeLo.setOnClickListener(View.OnClickListener {
+            showMealTypeBottomSheet()
+        })
     }
 
     private fun handleSubmitBtn() {
@@ -30,6 +44,36 @@ class CreateMealActivity : AppCompatActivity() {
         binding.backIv.setOnClickListener(View.OnClickListener {
             finish()
         })
+    }
+
+    private fun showMealTypeBottomSheet() {
+        val bottomSheetDialog = BottomSheetDialog(this)
+        val bottomSheetView: View =
+            LayoutInflater.from(this).inflate(R.layout.meal_type_bottom_sheet_dialog, null)
+        bottomSheetDialog.setContentView(bottomSheetView)
+        cancelBtn = bottomSheetDialog.findViewById<View>(R.id.cancel_btn) as Button
+        crossImage = bottomSheetDialog.findViewById<View>(R.id.cross_iv) as ImageView
+        okBtn = bottomSheetDialog.findViewById<View>(R.id.ok_btn) as Button
+        bottomSheetDialog.setOnShowListener { dialog ->
+            val bottomSheet =
+                (dialog as BottomSheetDialog).findViewById<View>(com.google.android.material.R.id.design_bottom_sheet)
+            bottomSheet?.setBackgroundResource(R.drawable.rounded_bottom_sheet_background)
+        }
+
+        cancelBtn.setOnClickListener(View.OnClickListener {
+            bottomSheetDialog.dismiss()
+        })
+
+        crossImage.setOnClickListener(View.OnClickListener {
+            bottomSheetDialog.dismiss()
+        })
+
+        okBtn.setOnClickListener(View.OnClickListener {
+            bottomSheetDialog.dismiss()
+            Toast.makeText(this, "Meal Added", Toast.LENGTH_SHORT).show()
+        })
+
+        bottomSheetDialog.show()
     }
 
 }
--- app/src/main/java/com/iprism/school/activities/EditMealPlannerActivity.kt
+++ app/src/main/java/com/iprism/school/activities/EditMealPlannerActivity.kt
@@ -3,6 +3,8 @@ package com.iprism.school.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Toast
 import com.iprism.school.R
 import com.iprism.school.databinding.ActivityEditMealPlannerBinding
 import com.iprism.school.databinding.ActivityMealPlannerBinding
@@ -10,9 +12,9 @@ import com.iprism.school.databinding.ActivityMealPlannerBinding
 class EditMealPlannerActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityEditMealPlannerBinding
-    private var foodId : String = ""
-    private var foodName : String = ""
-    private var remarks : String = ""
+    private var foodId: String = ""
+    private var foodName: String = ""
+    private var remarks: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +24,22 @@ class EditMealPlannerActivity : AppCompatActivity() {
         remarks = intent.getStringExtra("remarks").toString()
         setContentView(binding.root)
         Log.d("foodId", "$foodId, $foodName, $remarks")
+        handleBack()
+        handleUpdate()
+
+    }
+
+    private fun handleUpdate() {
+        binding.updateBtn.setOnClickListener(View.OnClickListener {
+            Toast.makeText(this, "Meal Updated Successfully", Toast.LENGTH_SHORT).show()
+            finish()
+        })
+    }
+
+    private fun handleBack() {
+        binding.backIv.setOnClickListener(View.OnClickListener {
+            finish()
+        })
     }
 
 }
