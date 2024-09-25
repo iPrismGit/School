4a757f48b3cd051c70f9e0c8133344ff51138f7e
CHARAN TEJA <charan.teja@improve10x.com>
2024-09-25 14:24:24 +0530

    Created Calender Edit Activity

--- app/src/main/java/com/iprism/school/fragments/DiaryFragment.kt
+++ app/src/main/java/com/iprism/school/fragments/DiaryFragment.kt
@@ -1,19 +1,32 @@
 package com.iprism.school.fragments
 
+import android.os.Binder
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.iprism.school.R
+import com.iprism.school.adapters.DairiesAdapter
+import com.iprism.school.databinding.FragmentDiaryBinding
 
 class DiaryFragment : Fragment() {
 
+    private lateinit var binding : FragmentDiaryBinding
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_diary, container, false)
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentDiaryBinding.inflate(inflater, container, false)
+        setupDiaryAdapter()
+        return binding.root
     }
+
+    private fun setupDiaryAdapter() {
+        var dairiesAdapter = DairiesAdapter(requireContext())
+        binding.dairyRv.adapter = dairiesAdapter
+        var layoutManager = LinearLayoutManager(requireContext())
+        binding.dairyRv.layoutManager = layoutManager
+    }
+
 }
