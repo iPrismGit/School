a91d695e7277d249c3e60e7c3e138f38497fbf19
CHARAN TEJA <charan.teja@improve10x.com>
2024-09-27 17:58:36 +0530

    Created Delivery Reports Activity

--- app/src/main/java/com/iprism/school/activities/DiaryDeliveryReportsActivity.kt
+++ app/src/main/java/com/iprism/school/activities/DiaryDeliveryReportsActivity.kt
@@ -1,8 +1,10 @@
 package com.iprism.school.activities
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.iprism.school.R
 import com.iprism.school.adapters.DeliveryReportsAdapter
@@ -11,7 +13,7 @@ import com.iprism.school.databinding.ActivityDiaryDeliveryReportsBinding
 class DiaryDeliveryReportsActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityDiaryDeliveryReportsBinding
-    var diaryId : String = ""
+    var diaryId: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +22,28 @@ class DiaryDeliveryReportsActivity : AppCompatActivity() {
         diaryId = intent.getStringExtra("diaryId").toString()
         setupDeliveryReportsAdapter()
         handleBack()
+        handleSearchLo()
+        handleCross()
+    }
+
+    private fun handleCross() {
+        binding.crossIv.setOnClickListener(View.OnClickListener {
+            binding.toolBarLo.visibility = View.VISIBLE
+            binding.searchLo.visibility = View.GONE
+            binding.searchTxt.setText("")
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(binding.searchTxt.windowToken, 0)
+        })
+    }
+
+    private fun handleSearchLo() {
+        binding.searchIv.setOnClickListener(View.OnClickListener {
+            binding.toolBarLo.visibility = View.GONE
+            binding.searchLo.visibility = View.VISIBLE
+            binding.searchTxt.requestFocus()
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.showSoftInput(binding.searchTxt, InputMethodManager.SHOW_IMPLICIT)
+        })
     }
 
     private fun handleBack() {
