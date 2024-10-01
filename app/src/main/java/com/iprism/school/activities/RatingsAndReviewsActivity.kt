9eaf12b4c1471096462baaa537210ec7d16e0ace
CHARAN TEJA <charan.teja@improve10x.com>
2024-10-01 15:54:24 +0530

    Implemented Ratings Function with Radio Buttons.

--- app/src/main/java/com/iprism/school/activities/RatingsAndReviewsActivity.kt
+++ app/src/main/java/com/iprism/school/activities/RatingsAndReviewsActivity.kt
@@ -2,17 +2,103 @@ package com.iprism.school.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.RadioButton
+import androidx.core.widget.NestedScrollView
+import com.google.android.material.tabs.TabLayoutMediator
 import com.iprism.school.R
+import com.iprism.school.adapters.ConsentsTabsAdapter
+import com.iprism.school.adapters.ReviewsPagerAdapter
 import com.iprism.school.databinding.ActivityRatingsAndReviewsBinding
 
 class RatingsAndReviewsActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityRatingsAndReviewsBinding
+    private var averageValue: Int = 0
+    private var infrastructrureValue: Int = 4
+    private var securityValue: Int = 3
+    private var curriculumAndActivitiesValue: Int = 2
+    private var communicationValue: Int = 5
+    private var staffValue: Int = 5
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRatingsAndReviewsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setUpTabs()
+        setSelectedRating(
+            infrastructrureValue,
+            binding.infrastructure1Rb,
+            binding.infrastructure2Rb,
+            binding.infrastructure3Rb,
+            binding.infrastructure4Rb,
+            binding.infrastructure5Rb
+        )
+        setSelectedRating(
+            securityValue,
+            binding.security1Rb,
+            binding.security2Rb,
+            binding.security3Rb,
+            binding.security4Rb,
+            binding.security5Rb
+        )
+        setSelectedRating(
+            curriculumAndActivitiesValue,
+            binding.curriculumAndActivities1Rb,
+            binding.curriculumAndActivities2Rb,
+            binding.curriculumAndActivities3Rb,
+            binding.curriculumAndActivities4Rb,
+            binding.curriculumAndActivities5Rb
+        )
+        setSelectedRating(
+            communicationValue,
+            binding.communication1Rb,
+            binding.communication2Rb,
+            binding.communication3Rb,
+            binding.communication4Rb,
+            binding.communication5Rb
+        )
+        setSelectedRating(
+            staffValue,
+            binding.staff1Rb,
+            binding.staff2Rb,
+            binding.staff3Rb,
+            binding.staff4Rb,
+            binding.staff5Rb
+        )
+        setSelectedRating(
+            18 / 5,
+            binding.average1Rb,
+            binding.average2Rb,
+            binding.average3Rb,
+            binding.average4Rb,
+            binding.average5Rb
+        )
+    }
+
+    private fun setUpTabs() {
+        val reviewsPagerAdapter = ReviewsPagerAdapter(this)
+        binding.viewPager.adapter = reviewsPagerAdapter
+        val tabLayoutMediator =
+            TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+                when (position) {
+                    0 -> tab.text = "Pending"
+                    1 -> tab.text = "Approved"
+                    else -> tab.text = "Rejected"
+                }
+            }
+        tabLayoutMediator.attach()
+    }
+
+    private fun setSelectedRating(
+        ratingValue: Int,
+        vararg radioButtons: RadioButton
+    ) {
+
+        radioButtons.forEachIndexed { index, radioButton ->
+            radioButton.isChecked = index < ratingValue
+            radioButton.isClickable = false
+        }
     }
 
 }
--- app/src/main/java/com/iprism/school/adapters/ReviewsPagerAdapter.kt
+++ app/src/main/java/com/iprism/school/adapters/ReviewsPagerAdapter.kt
@@ -4,7 +4,10 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.iprism.school.fragments.ActiveConsentsFragment
+import com.iprism.school.fragments.ApprovedReviewsFragment
 import com.iprism.school.fragments.InactiveConsentsFragment
+import com.iprism.school.fragments.PendingReviewsFragment
+import com.iprism.school.fragments.RejectedReviewsFragment
 
 class ReviewsPagerAdapter(activity: FragmentActivity) : FragmentStateAdapter(activity) {
     override fun getItemCount(): Int {
@@ -13,9 +16,11 @@ class ReviewsPagerAdapter(activity: FragmentActivity) : FragmentStateAdapter(act
 
     override fun createFragment(position: Int): Fragment {
         return when (position) {
-            0 -> ActiveConsentsFragment()
-            1 -> InactiveConsentsFragment()
+            0 -> PendingReviewsFragment()
+            1 -> ApprovedReviewsFragment()
+            2 -> RejectedReviewsFragment()
             else -> throw IllegalArgumentException("Invalid position")
         }
     }
+
 }
