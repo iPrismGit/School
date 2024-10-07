483c236687a25e3b2da8e7edc0aff1763fa170eb
Omprakash <omprakash.mopuri@improve10x.com>
2024-10-07 16:54:14 +0530

    Completed bug

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,4 @@
 
-buildscript {
-    repositories {
-        google()
-        jcenter()
-        mavenCentral()
-    }
-    dependencies {
-        //firebase
-        classpath ("com.google.gms:google-services:4.3.15")
-    }
-}
-
 plugins {
     id("com.android.application") version "8.1.0" apply false
     id("org.jetbrains.kotlin.android") version "1.8.0" apply false
