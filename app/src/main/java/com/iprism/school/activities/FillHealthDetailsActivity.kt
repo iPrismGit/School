4533314cddd1052125568180903eb76ae11a7103
CHARAN TEJA <charan.teja@improve10x.com>
2024-10-11 09:59:52 +0530

    Created Add Student has had Bottom Sheet And Designed it

--- app/src/main/java/com/iprism/school/activities/FillHealthDetailsActivity.kt
+++ app/src/main/java/com/iprism/school/activities/FillHealthDetailsActivity.kt
@@ -2,10 +2,18 @@ package com.iprism.school.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.iprism.school.R
 import com.iprism.school.databinding.ActivityFillHealthDetailsBinding
 import com.iprism.school.databinding.ActivityFillOtherDetailsBinding
+import com.iprism.school.databinding.AddMoreBottomSheetLayoutBinding
+import com.iprism.school.databinding.ChildHasHadBottomSheetBinding
+import com.iprism.school.databinding.ChildSuffersFromBottomSheetBinding
 import com.iprism.school.utils.ToastUtils
 
 class FillHealthDetailsActivity : AppCompatActivity() {
@@ -18,6 +26,27 @@ class FillHealthDetailsActivity : AppCompatActivity() {
         setContentView(binding.root)
         handleBackBtn()
         handleAddStudentBtn()
+        handleAddMoreBtn()
+        handleChildHasHadLo()
+        handleChildSuffersFromLo()
+    }
+
+    private fun handleChildHasHadLo() {
+        binding.childHasHadLo.setOnClickListener(View.OnClickListener {
+            showChildHasHadBottomSheet()
+        })
+    }
+
+    private fun handleChildSuffersFromLo() {
+        binding.childSuffersFromLo.setOnClickListener(View.OnClickListener {
+            showChildSuffersFromBottomSheet()
+        })
+    }
+
+    private fun handleAddMoreBtn() {
+        binding.addBtn.setOnClickListener(View.OnClickListener {
+            showAddMoreBottomSheet()
+        })
     }
 
     private fun handleAddStudentBtn() {
@@ -32,5 +61,77 @@ class FillHealthDetailsActivity : AppCompatActivity() {
         })
     }
 
+
+    private fun showChildHasHadBottomSheet() {
+        val bottomSheetDialog = BottomSheetDialog(this)
+        val binding = ChildHasHadBottomSheetBinding.inflate(layoutInflater)
+        bottomSheetDialog.setContentView(binding.root)
+        bottomSheetDialog.setOnShowListener { dialog ->
+            val bottomSheet =
+                (dialog as BottomSheetDialog).findViewById<View>(com.google.android.material.R.id.design_bottom_sheet)
+            bottomSheet?.setBackgroundResource(R.drawable.rounded_bottom_sheet_background)
+            binding.okBtn.setOnClickListener(View.OnClickListener {
+                bottomSheetDialog.dismiss()
+                ToastUtils.showSuccessCustomToast(this, "More Added Successfully")
+            })
+
+            binding.crossIv.setOnClickListener(View.OnClickListener {
+                bottomSheetDialog.dismiss()
+            })
+
+            binding.cancelBtn.setOnClickListener(View.OnClickListener {
+                bottomSheetDialog.dismiss()
+            })
+        }
+        bottomSheetDialog.show()
+    }
+
+    private fun showChildSuffersFromBottomSheet() {
+        val bottomSheetDialog = BottomSheetDialog(this)
+        val binding = ChildSuffersFromBottomSheetBinding.inflate(layoutInflater)
+        bottomSheetDialog.setContentView(binding.root)
+        bottomSheetDialog.setOnShowListener { dialog ->
+            val bottomSheet =
+                (dialog as BottomSheetDialog).findViewById<View>(com.google.android.material.R.id.design_bottom_sheet)
+            bottomSheet?.setBackgroundResource(R.drawable.rounded_bottom_sheet_background)
+            binding.okBtn.setOnClickListener(View.OnClickListener {
+                bottomSheetDialog.dismiss()
+                ToastUtils.showSuccessCustomToast(this, "More Added Successfully")
+            })
+
+            binding.crossIv.setOnClickListener(View.OnClickListener {
+                bottomSheetDialog.dismiss()
+            })
+
+            binding.cancelBtn.setOnClickListener(View.OnClickListener {
+                bottomSheetDialog.dismiss()
+            })
+        }
+        bottomSheetDialog.show()
+    }
+
+    private fun showAddMoreBottomSheet() {
+        val bottomSheetDialog = BottomSheetDialog(this)
+        val binding = AddMoreBottomSheetLayoutBinding.inflate(layoutInflater)
+        bottomSheetDialog.setContentView(binding.root)
+        bottomSheetDialog.setOnShowListener { dialog ->
+            val bottomSheet =
+                (dialog as BottomSheetDialog).findViewById<View>(com.google.android.material.R.id.design_bottom_sheet)
+            bottomSheet?.setBackgroundResource(R.drawable.rounded_bottom_sheet_background)
+            binding.okBtn.setOnClickListener(View.OnClickListener {
+                bottomSheetDialog.dismiss()
+                ToastUtils.showSuccessCustomToast(this, "More Added Successfully")
+            })
+
+            binding.crossIv.setOnClickListener(View.OnClickListener {
+                bottomSheetDialog.dismiss()
+            })
+
+            binding.cancelBtn.setOnClickListener(View.OnClickListener {
+                bottomSheetDialog.dismiss()
+            })
+        }
+        bottomSheetDialog.show()
+    }
 
 }
