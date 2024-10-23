5bccee531a67110c13bc21706b11a421921a638d
CHARAN TEJA <charan.teja@improve10x.com>
2024-10-23 09:43:05 +0530

    Created Albums Activity And Designed.

---
+++ app/src/main/java/com/iprism/school/activities/AlbumDetailsActivity.kt
@@ -0,0 +1,18 @@
+package com.iprism.school.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.iprism.school.R
+import com.iprism.school.databinding.ActivityAlbumDetailsBinding
+
+class AlbumDetailsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAlbumDetailsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAlbumDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+}
