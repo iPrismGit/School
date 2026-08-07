0227d5fd26ba063d3da25c7f1b098658e795ee26
Omprakash Mopuri <mopuri.omprakash@gmail.com>
2026-08-07 10:10:23 +0530

    Refactored ConsentsActivity pagination and CircularsAdapter.
    - Implemented a loading footer in `CircularsAdapter` using multiple view types.
    - Updated `ConsentsActivity` to handle pagination more efficiently and removed unused `AttendanceViewModel` dependencies.
    - Improved scroll listener logic to trigger data fetching for subsequent pages.
    - Refined `UiState` observation to manage progress indicators and empty states.

--- app/src/main/java/com/iprism/school/activities/ConsentsActivity.kt
+++ app/src/main/java/com/iprism/school/activities/ConsentsActivity.kt
@@ -1,5 +1,6 @@
 package com.iprism.school.activities
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
@@ -8,36 +9,30 @@ import androidx.activity.enableEdgeToEdge
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.iprism.school.adapters.CircularsAdapter
 import com.iprism.school.base.BaseActivity
 import com.iprism.school.databinding.ActivityConsentsBinding
 import com.iprism.school.interfaces.OnCalenderClickListener
 import com.iprism.school.model.circularmodels.Circular
 import com.iprism.school.model.circularmodels.CircularApiRequest
-import com.iprism.school.repositories.AttendanceRepository
 import com.iprism.school.repositories.CircularRepository
 import com.iprism.school.utils.ToastUtils
 import com.iprism.school.utils.UiState
 import com.iprism.school.utils.User
 import com.iprism.school.utils.hideProgress
 import com.iprism.school.utils.showProgress
-import com.iprism.school.viewModels.AttendanceViewModel
 import com.iprism.school.viewModels.CircularViewModel
 import com.iprism.school.viewModels.ViewModelFactory
 
 class ConsentsActivity : BaseActivity() {
 
     private lateinit var binding: ActivityConsentsBinding
-    private lateinit var attendanceViewModel: AttendanceViewModel
-    private lateinit var circularViewModel: CircularViewModel
-    private lateinit var circularsAdapter: CircularsAdapter
-    private var circularList = mutableListOf<Circular>()
-    private var isFreshLoad = false
+    private lateinit var viewModel: CircularViewModel
     private var isLoading = false
     private var isLastPage = false
     private var currentPage = 1
-    private val limit = 10
+    private var circularsItems = mutableListOf<Circular>()
+    private lateinit var circularsAdapter: CircularsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,196 +40,10 @@ class ConsentsActivity : BaseActivity() {
         setContentView(binding.root)
         enableEdgeToEdge()
         handleBack()
+        setUpCirculars()
         initViewModel()
-        setupRecyclerView()
-        observeCircularsResponse()
-        handleRefreshLo()
-        val request = CircularApiRequest(
-            userDetails[User.Companion.ACADEMIC_YEAR_ID].toString(),
-            userDetails[User.Companion.SCHOOL_ID].toString(),
-            currentPage,
-            userDetails[User.Companion.ID].toString()
-        )
-
-        Log.d("CircularApiRequest", request.toString())
-        circularViewModel.fetchCirculars(request)
-    }
-
-    private fun loadEvents(isFromFilterChange: Boolean = false) {
-
-        if (isLoading) return
-
-        if (isFromFilterChange) {
-            currentPage = 1
-            isLastPage = false
-            isFreshLoad = true
-
-            circularList.clear()
-            circularsAdapter.notifyDataSetChanged()
-
-            binding.circularsRv.visibility = View.GONE
-            binding.noDataFoundTxt.visibility = View.VISIBLE
-        }
-
-        isLoading = true
-
-        val request = CircularApiRequest(
-            userDetails[User.Companion.ACADEMIC_YEAR_ID].toString(),
-            userDetails[User.Companion.SCHOOL_ID].toString(),
-            currentPage,
-            userDetails[User.Companion.ID].toString()
-        )
-
-        Log.d("CircularApiRequest", request.toString())
-        circularViewModel.fetchCirculars(request)
-    }
-
-    private fun refreshItems() {
-        currentPage = 1
-        isLastPage = false
-        isLoading = false
-        circularList.clear()
-        circularsAdapter.notifyDataSetChanged()
-        loadEvents(isFromFilterChange = true)
-    }
-
-    private fun loadMoreItems() {
-        if (isLastPage || isLoading) return
-        isLoading = true
-        currentPage++
-        loadEvents()
-    }
-
-    private fun resetEvents() {
-        currentPage = 1
-        isLastPage = false
-        isLoading = false
-
-        circularList.clear()
-        circularsAdapter.notifyDataSetChanged()
-
-        binding.circularsRv.visibility = View.GONE
-        binding.noDataFoundTxt.visibility = View.VISIBLE
-    }
-
-    private fun handleRefreshLo() {
-        binding.refreshLayout.setOnRefreshListener(
-            SwipeRefreshLayout.OnRefreshListener {
-                refreshItems()
-                binding.refreshLayout.isRefreshing = false
-            }
-        )
-    }
-
-    private fun setupRecyclerView() {
-        circularsAdapter = CircularsAdapter(this, circularList)
-        val linearLayoutManager = LinearLayoutManager(this)
-
-        binding.circularsRv.apply {
-            layoutManager = linearLayoutManager
-            adapter = circularsAdapter
-
-            addOnScrollListener(object : RecyclerView.OnScrollListener() {
-                override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-                    super.onScrolled(recyclerView, dx, dy)
-                    binding.refreshLayout.isEnabled =
-                        !binding.circularsRv.canScrollVertically(-1)
-                    val visibleItemCount = linearLayoutManager.childCount
-                    val totalItemCount = linearLayoutManager.itemCount
-                    val firstVisibleItemPosition =
-                        linearLayoutManager.findFirstVisibleItemPosition()
-
-                    if (!isLoading && !isLastPage && circularList.isNotEmpty()) {
-                        if ((visibleItemCount + firstVisibleItemPosition) >= totalItemCount
-                            && firstVisibleItemPosition >= 0
-                        ) {
-                            loadMoreItems()
-                        }
-                    }
-
-                }
-            })
-
-            circularsAdapter.setupListener(object : OnCalenderClickListener {
-                override fun onItemClick(
-                    calenderId: String,
-                    calenderName: String,
-                    image: String
-                ) {
-                    if (image != null && image.isNotEmpty()) {
-                        val intent = Intent(this@ConsentsActivity, ViewImageActivity::class.java)
-                        intent.putExtra("EventImage", image)
-                        intent.putExtra("EventName", calenderName)
-                        startActivity(intent)
-                    } else {
-                        ToastUtils.showErrorCustomToast(this@ConsentsActivity, "No Image Found..!")
-                    }
-                }
-
-            })
-
-        }
-
-    }
-
-    private fun observeCircularsResponse() {
-        circularViewModel.circularsResponse.observe(this) { result ->
-
-            when (result) {
-                is UiState.Loading -> {
-                    if (currentPage == 1) {
-                        binding.progress.showProgress()
-                    }
-                }
-
-                is UiState.Success -> {
-                    binding.progress.hideProgress()
-                    isLoading = false
-
-                    val newCirculars = result.data.circulars
-
-                    if (newCirculars.isNotEmpty()) {
-
-                        if (isFreshLoad) {
-                            circularList.clear()
-                            isFreshLoad = false
-                        }
-
-                        circularList.addAll(newCirculars)
-                        circularsAdapter.notifyDataSetChanged()
-
-                        binding.circularsRv.visibility = View.VISIBLE
-                        binding.noDataFoundTxt.visibility = View.GONE
-
-                        isLastPage = newCirculars.size < limit
-                    } else {
-                        isLastPage = true
-
-                        if (currentPage == 1) {
-                            circularList.clear()
-                            circularsAdapter.notifyDataSetChanged()
-                            binding.circularsRv.visibility = View.GONE
-                            binding.noDataFoundTxt.visibility = View.VISIBLE
-                        }
-                    }
-
-                }
-
-                is UiState.Error -> {
-                    isLoading = false
-                    binding.progress.hideProgress()
-                    if (circularList.isEmpty()) {
-                        binding.circularsRv.visibility = View.GONE
-                        binding.noDataFoundTxt.visibility = View.VISIBLE
-                        ToastUtils.showErrorCustomToast(this, result.message)
-                    } else {
-                        binding.circularsRv.visibility = View.VISIBLE
-                        binding.noDataFoundTxt.visibility = View.GONE
-                        ToastUtils.showErrorCustomToast(this, "There is no more data")
-                    }
-                }
-            }
-        }
+        observeResponse()
+        fetchCirculars()
     }
 
     private fun handleBack() {
@@ -243,14 +52,104 @@ class ConsentsActivity : BaseActivity() {
         })
     }
 
-    private fun initViewModel() {
-        val repository = AttendanceRepository(this)
-        val factory = ViewModelFactory { AttendanceViewModel(repository) }
-        attendanceViewModel = ViewModelProvider(this, factory)[AttendanceViewModel::class.java]
-
-        val circularsRepository = CircularRepository(this)
-        val circularFactory = ViewModelFactory { CircularViewModel(circularsRepository) }
-        circularViewModel = ViewModelProvider(this, circularFactory)[CircularViewModel::class.java]
+    private fun setUpCirculars() {
+        circularsAdapter = CircularsAdapter(circularsItems as ArrayList<Circular?>)
+        val linearLayoutManager = LinearLayoutManager(this)
+        binding.circularsRv.apply {
+            layoutManager = linearLayoutManager
+            adapter = circularsAdapter
+            addOnScrollListener(object : RecyclerView.OnScrollListener() {
+                override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                    super.onScrolled(recyclerView, dx, dy)
+                    val visibleItemCount = linearLayoutManager.childCount
+                    val totalItemCount = linearLayoutManager.itemCount
+                    val firstVisibleItemPosition =
+                        linearLayoutManager.findFirstVisibleItemPosition()
+                    if (!isLoading && !isLastPage) {
+                        if ((visibleItemCount + firstVisibleItemPosition) >= totalItemCount && firstVisibleItemPosition >= 0) {
+                            loadMoreDoctors()
+                        }
+                    }
+                }
+            })
+        }
+        circularsAdapter.setupListener(object : OnCalenderClickListener {
+            override fun onItemClick(
+                calenderId: String,
+                calenderName: String,
+                image: String
+            ) {
+                if (image != null && image.isNotEmpty()){
+                    val intent = Intent(this@ConsentsActivity, ViewImageActivity::class.java)
+                    intent.putExtra("EventImage", image)
+                    intent.putExtra("EventName", calenderName)
+                    startActivity(intent)
+                }else{
+                    ToastUtils.showErrorCustomToast(this@ConsentsActivity, "No Image Found..!")
+                }
+            }
+        })
     }
 
+
+    private fun initViewModel() {
+        val repository = CircularRepository(this)
+        val factory = ViewModelFactory { CircularViewModel(repository) }
+        viewModel = ViewModelProvider(this, factory)[CircularViewModel::class.java]
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    private fun observeResponse() {
+        viewModel.circularsResponse.observe(this) { result ->
+            when (result) {
+                is UiState.Loading -> {
+                    if (currentPage == 1) {
+                        binding.progress.showProgress()
+                    }
+                }
+
+                is UiState.Success -> {
+                    binding.noDataFoundTxt.visibility = View.GONE
+                    binding.progress.hideProgress()
+                    isLoading = false
+                    circularsAdapter.removeLoadingFooter()
+                    val newBookings = result.data.circulars
+                    if (newBookings.isNotEmpty()) {
+                        circularsItems.addAll(newBookings)
+                        circularsAdapter.notifyDataSetChanged()
+                        if (result.data.pagination.total_pages.size == currentPage) {
+                            isLastPage = true
+                        }
+                    }
+                }
+
+                is UiState.Error -> {
+                    isLoading = false
+                    circularsAdapter.removeLoadingFooter()
+                    binding.progress.hideProgress()
+                    if (result.message.equals("no data found", true)) {
+                        binding.noDataFoundTxt.visibility = View.VISIBLE
+                    }
+                }
+            }
+        }
+    }
+
+    private fun fetchCirculars() {
+        val request = CircularApiRequest(
+            userDetails[User.Companion.ACADEMIC_YEAR_ID].toString(),
+            userDetails[User.Companion.SCHOOL_ID].toString(),
+            currentPage,
+            userDetails[User.Companion.ID].toString()
+        )
+        viewModel.fetchCirculars(request)
+        Log.d("requestLoading", request.toString())
+    }
+
+    private fun loadMoreDoctors() {
+        isLoading = true
+        currentPage += 1
+        circularsAdapter.showLoadingFooter()
+        fetchCirculars()
+    }
 }
--- app/src/main/java/com/iprism/school/adapters/CircularsAdapter.kt
+++ app/src/main/java/com/iprism/school/adapters/CircularsAdapter.kt
@@ -1,14 +1,15 @@
 package com.iprism.school.adapters
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.iprism.school.databinding.CircularItemBinding
+import com.iprism.school.databinding.ItemLoadingBinding
 import com.iprism.school.interfaces.OnCalenderClickListener
 import com.iprism.school.model.circularmodels.Circular
+import com.iprism.school.viewholders.ItemLoadingViewHolder
 
-class CircularsAdapter(var activity: Context, private val circulars: List<Circular>) : RecyclerView.Adapter<CircularsAdapter.ViewHolders>() {
+class CircularsAdapter(private val circulars: ArrayList<Circular?>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
    private lateinit var listener: OnCalenderClickListener
 
@@ -16,28 +17,56 @@ class CircularsAdapter(var activity: Context, private val circulars: List<Circul
         this.listener = listener
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolders {
-        val binding = CircularItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return ViewHolders(binding)
+    companion object {
+        private const val VIEW_TYPE_ITEM = 1
+        private const val VIEW_TYPE_LOADING = 0
     }
 
-    override fun onBindViewHolder(holder: ViewHolders, position: Int) {
-        val circular = circulars[position]
-        holder.binding.nameTxt.text  = circular.title
-        holder.binding.dateTxt.text  = "Date : " + circular.created_on
-        if(circular.description != null && circular.description.isNotEmpty()){
-            holder.binding.detailsTxt.text  = "Notes : " + circular.description
-        } else{
-            holder.binding.detailsTxt.text  = "Notes : " + "Not Given"
-        }
+    override fun getItemViewType(position: Int): Int {
+        return if (circulars[position] == null) VIEW_TYPE_LOADING else VIEW_TYPE_ITEM
+    }
 
-        holder.binding.imageViewIv.setOnClickListener { view ->
-            listener.onItemClick(circular.id, circular.title, circular.image)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val binding = CircularItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val itemLoadingBinding = ItemLoadingBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return if (viewType == VIEW_TYPE_ITEM) {
+            ViewHolders(binding)
+        } else {
+            ItemLoadingViewHolder(itemLoadingBinding)
         }
+    }
 
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (holder is ViewHolders) {
+            val circular = circulars[position]
+            holder.binding.nameTxt.text = circular!!.title
+            holder.binding.dateTxt.text = "Date : " + circular.created_on
+            if (circular.description != null && circular.description.isNotEmpty()) {
+                holder.binding.detailsTxt.text = "Notes : " + circular.description
+            } else {
+                holder.binding.detailsTxt.text = "Notes : " + "Not Given"
+            }
+
+            holder.binding.imageViewIv.setOnClickListener { view ->
+                listener.onItemClick(circular.id, circular.title, circular.image)
+            }
+        }
     }
 
     override fun getItemCount(): Int = circulars.size
+
+    fun showLoadingFooter() {
+        circulars.add(null)
+        notifyItemInserted(circulars.size - 1)
+    }
+
+    fun removeLoadingFooter() {
+        val index = circulars.indexOf(null)
+        if (index != -1) {
+            circulars.removeAt(index)
+            notifyItemRemoved(index)
+        }
+    }
 
 
     class ViewHolders(var binding: CircularItemBinding) : RecyclerView.ViewHolder(binding.root) {
